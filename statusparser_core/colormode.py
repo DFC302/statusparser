6a03267fa3c7b0a3e9f934d8fd1abf4cdc2d98f7
Matthew Greer <pydev302@gmail.com>
2020-03-06 00:02:27 -0500

    Update colormode.py

--- statusparser_core/colormode.py
+++ statusparser_core/colormode.py
@@ -61,7 +61,7 @@ class StatusParserColorMode():
 			# If URL has invalid characterrs in it, replace
 			urls = [url.strip("\n").replace("__", "://").replace("_", ".").replace("*.", "http://") for url in f] # took out + /
 			urls = [url for url in urls if not url.startswith("#")]
-			# If url does not start with http://, add it, redirect will handle the rest
+			# If url does not start with http://
 			urls = [f"http://{url}" for url in urls if not url.startswith("http")]
 
 			# Let user choose threads, else use default of 20
